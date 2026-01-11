ecbe6c2f16def92aa99ce5f75053fed40cc3ecd7
GATTAL Hamza <hmzgtl16@gmail.com>
2026-01-11 14:32:37 +0100

    Implement `ProductRepository` as `CoroutineCrudRepository` with `ProductEntity`

--- server/src/main/kotlin/com/example/hshop/api/repository/ProductRepository.kt
+++ server/src/main/kotlin/com/example/hshop/api/repository/ProductRepository.kt
@@ -1,4 +1,8 @@
 package com.example.hshop.api.repository
 
-interface ProductRepository {
+import com.example.hshop.api.entity.ProductEntity
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import java.util.UUID
+
+interface ProductRepository : CoroutineCrudRepository<ProductEntity, UUID> {
 }
